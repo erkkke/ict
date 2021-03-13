b9040963c06b4b6f71faab1aaf6f0536a242ad2c
Erkebulan <60324363+erkkke@users.noreply.github.com>
2021-03-13 15:38:30 +0600

    Add files via upload

---
+++ FarManager/FarManager/Layer.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FarManager
+{
+    class Layer
+    {
+        public DirectoryInfo dir
+        {
+            get;
+            set;
+        }
+        public int pos
+        {
+            get;
+            set;
+        }
+        public List<FileSystemInfo> content
+        {
+            get;
+            set;
+        }
+
+        float size = 0.0f;
+
+        public Layer(DirectoryInfo dir, int pos)
+        {
+            this.dir = dir;
+            this.pos = pos;
+            refreshDirectoryList();
+        }
+
+    
+
+        public void PrintInfo()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            int cnt = 0;
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                if (cnt == pos)
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                
+                else
+                    Console.BackgroundColor = ConsoleColor.Black;
+                
+                Console.WriteLine($"{cnt + 1}. {d.Name}");
+                cnt++;
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            foreach (FileInfo f in dir.GetFiles())
+            {
+                if (cnt == pos)
+                {
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine($"{cnt + 1}. {f.Name}");
+                cnt++;
+            }
+        }
+
+        void CalculateSize(string path)
+        {
+            if (GetCurrentObject().GetType() == typeof(DirectoryInfo)) {
+                DirectoryInfo tempDir = new DirectoryInfo(path);
+                foreach (FileSystemInfo fi in tempDir.GetFileSystemInfos())
+                {
+                    if (fi.GetType() == typeof(DirectoryInfo))
+                        CalculateSize(fi.FullName);
+                    else
+                    {
+                        FileInfo f = fi as FileInfo;
+                        this.size += f.Length;
+                    }
+                }
+            }
+            else
+            {
+                FileInfo tempFile = new FileInfo(content[pos].FullName);
+                this.size += tempFile.Length;
+            }
+        }
+
+
+        public void ShowSize()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+            this.size = 0;
+            CalculateSize(GetCurrentObject().FullName);
+            Console.WriteLine("\nSize of \"" + content[pos].Name + "\" is " + this.size + " Bytes");
+        }
+
+        public FileSystemInfo GetCurrentObject()
+        {
+            return content[pos];
+        }
+
+        public void DisplayFileContent(string path)
+        {
+            Console.Clear();
+            using (StreamReader file = new StreamReader(path))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+
+        public void SetNewPosition(int d)
+        {
+            if (d > 0)
+                pos++;
+            else
+                pos--;
+            
+            if (pos >= content.Count)
+                pos = 0;
+            else if (pos < 0)
+                pos = content.Count - 1;
+        }
+
+        public void CreateFolder()
+        {
+            string newFolder = Console.ReadLine();
+            string path = Path.Combine(dir.Parent.FullName, newFolder);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                refreshDirectoryList();
+            }
+            else
+                Console.WriteLine("Directory is already exist");
+        }
+        public void CreateFile()
+        {
+            string newFile = Console.ReadLine();
+            string path = Path.Combine(dir.FullName, newFile);
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+                refreshDirectoryList();
+            }
+                
+            else
+                Console.WriteLine("File is already exist");
+        }
+
+        public void DeleteFile()
+        {
+            if (GetCurrentObject() is FileInfo)
+            {
+                string path = GetCurrentObject().FullName;
+                File.Delete(path);
+                pos -= 1;
+                refreshDirectoryList();
+            }
+        }
+        void refreshDirectoryList()
+        {
+            this.content = new List<FileSystemInfo>();
+
+            content.AddRange(this.dir.GetDirectories());
+            content.AddRange(this.dir.GetFiles());
+        }
+    }
+}
---
+++ FarManager/FarManager/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FarManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FarManager();
+        }
+
+        private static void FarManager()
+        {
+
+            Stack<Layer> history = new Stack<Layer>();
+            history.Push(new Layer(new DirectoryInfo(@"C:\CODE"), 0));
+
+            bool escape = false;
+            bool isFileOpened = false;
+
+            while (!escape)
+            {
+                Console.Clear();
+
+                if (!isFileOpened)
+                {
+                    history.Peek().PrintInfo();
+                    history.Peek().ShowSize();
+                }
+                else 
+                    history.Peek().DisplayFileContent(history.Peek().GetCurrentObject().FullName);
+
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
+                Console.CursorVisible = false;
+
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.Enter:
+                        if (history.Peek().GetCurrentObject().GetType() == typeof(DirectoryInfo))
+                            history.Push(new Layer(history.Peek().GetCurrentObject() as DirectoryInfo, 0));
+                        else
+                            isFileOpened = true;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        history.Peek().SetNewPosition(-1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        history.Peek().SetNewPosition(1);
+                        break;
+                    case ConsoleKey.Escape:
+                        if (history.Count > 1 && !isFileOpened)
+                            history.Pop();
+                        else if (isFileOpened)
+                            isFileOpened = false;
+                        else
+                            escape = true;
+                        break;
+                    case ConsoleKey.Spacebar:
+                        history.Peek().CreateFile();
+                        break;
+                    case ConsoleKey.Q:
+                        history.Peek().DeleteFile();
+                        break;
+                }
+            }
+        }
+    }
+}
